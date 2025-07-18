88906feb1824cbfc928d167c232af9dbec4ee8f1
Mira Littmann <miralittmann@ap23.uc.osg-htc.org>
2025-07-18 08:55:45 -0500

    help me mark you're my only hope

--- bib_trends_analysis.py
+++ bib_trends_analysis.py
@@ -9,6 +9,7 @@ import os
 import pickle
 from matplotlib.backends.backend_pdf import PdfPages
 import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 # Currently doing this for just 4TeV, 10ns staus in the medium time window but with increasing amounts of BIB
 # to see how efficiency scales
@@ -28,8 +29,15 @@ def get_chunk_id(fname: str) -> int:
 all_eff_data = {
     option: {
         "accepted": [],
+        "eff_precut": [],
         "total_eff": [],
-        "percent_good_tracks": []
+        "percent_good_tracks": [],
+        "loss_chi2": [],
+        "loss_reco": [],
+        "total_matched": [],
+        "eta_eff": [],
+        "eta_err": [],
+        "eta_centers": []
     } for option in bib_options
 }
 for option in bib_options: 
@@ -42,6 +50,10 @@ for option in bib_options:
 
     print(f"\n=================={option} percent BIB:======================")
 
+    eta_bins = np.linspace(-1.01, 1.01, 21)
+    eta_den = np.zeros(len(eta_bins)-1, int)
+    eta_num = np.zeros(len(eta_bins)-1, int) 
+ 
     for reco_file in os.listdir(reco_dir):
         reco_path = os.path.join(reco_dir, reco_file)
         if not os.path.isfile(reco_path):
@@ -60,13 +72,28 @@ for option in bib_options:
         bad_reco_tracks += bad_mask.sum()
         good_reco_tracks += good_mask.sum() 
 
+        theta_truth = np.asarray(reco_data["match_track_info"]["theta"])
+        eta_truth = np.asarray(-np.log(np.tan(0.5*theta_truth)))
+        eta_den += np.histogram(eta_truth, bins=eta_bins)[0]
+        eta_reco = eta_truth[good_mask]
+        eta_num += np.histogram(eta_reco, bins=eta_bins)[0]
+    eta_eff = np.divide(eta_num, eta_den, out=np.zeros_like(eta_num, float), where=eta_den>0)
+    eta_err = np.zeros_like(eta_eff)
+    eta_m = eta_den > 0
+    eta_err[eta_m] = np.sqrt(eta_eff[eta_m]* (1-eta_eff[eta_m])/eta_den[eta_m])
+    eta_centers = 0.5*(eta_bins[1:] + eta_bins[:-1])
+
+    all_eff_data[option]["eta_err"]=eta_err*100
+    all_eff_data[option]["eta_eff"]=eta_eff*100
+    all_eff_data[option]["eta_centers"]=eta_centers
 
     print(f"found {good_reco_tracks} tracks with reduced chi^2 < 5")
     print(f"and {bad_reco_tracks} with reduced chi^2 > 5 :(")
     percent_good_track = good_reco_tracks / (good_reco_tracks + bad_reco_tracks) * 100
-    all_eff_data[option]["percent_good_tracks"].append(percent_good_track)
+    all_eff_data[option]["percent_good_tracks"]=(percent_good_track)
     total_tracks = good_reco_tracks + bad_reco_tracks
     print('total tracks', total_tracks)
+     
  
     for sim_file in os.listdir(sim_dir):
         chunk_id = get_chunk_id(sim_file)
@@ -88,11 +115,12 @@ for option in bib_options:
     all_eff_data[option]["accepted"].append(total_accepted_staus)
     print("all accepted", total_accepted_staus)
     tot_eff = good_reco_tracks / total_accepted_staus * 100
-    all_eff_data[option]["total_eff"].append(tot_eff)
+    all_eff_data[option]["total_eff"]=(tot_eff)
+    all_eff_data[option]["loss_reco"]=((total_accepted_staus - (good_reco_tracks + bad_reco_tracks))/total_accepted_staus)*100
+    all_eff_data[option]["eff_precut"]=((good_reco_tracks + bad_reco_tracks)/total_accepted_staus * 100)
+    all_eff_data[option]["loss_chi2"]=(bad_reco_tracks / total_accepted_staus * 100)
 
     print(f"total efficiency is {tot_eff}")
-
-
 
 with PdfPages(plot_path) as pdf:
     efficiencies = []
@@ -122,15 +150,46 @@ with PdfPages(plot_path) as pdf:
     pdf.savefig(fig1)
     plt.close(fig1)
 
+    accepted_total = [all_eff_data[option]["accepted"] for option in bib_options]
+    eff_precut = [all_eff_data[option]["eff_precut"] for option in bib_options]
+    eff_total = [all_eff_data[option]["total_eff"] for option in bib_options]
+
     fig2, ax2 = plt.subplots(figsize=(6,5))
-    ax2.plot(percents, 1/(np.asarray(good_tracks) / np.asarray(efficiencies)), linestyle = '--', marker=".", color="tab:blue")
+    ax2.plot(bib_options, eff_precut, label="before $\chi^2$ cut")
+    ax2.plot(bib_options, eff_total, label="after $\chi^2$ cut")
     ax2.set_xlabel("Percent of BIB included (%)")
-    ax2.set_ylabel("Percent of tracks with red chi^2 < 5 / Reconstruction efficiency")
-    ax2.set_title("Percent of 'good' tracks reconstructed")
+    ax2.set_ylabel("Reco efficiency (%)")
+    ax2.set_title("Reco tracking efficiency before/after $\chi^2$ cut")
+    ax2.legend()
     fig2.tight_layout()
     pdf.savefig(fig2)
     plt.close(fig2)
 
+    loss_chi2_frac = [all_eff_data[p]["loss_chi2"] for p in bib_options]
+    loss_reco_frac = [all_eff_data[p]["loss_reco"] for p in bib_options]
+    loss_share = [lc/(lc+lr)*100 for lc, lr in zip(loss_chi2_frac, loss_reco_frac)]
+        
+    fig3, ax3 = plt.subplots(figsize=(6,5))
+    ax3.plot(bib_options, loss_share, marker='o')
+    ax3.set_xlabel("Percent of BIB (%)")
+    ax3.set_ylabel("$\chi^2$-cut share of total tracking loss")
+    ax3.set_title("How much of total lost reconstruction efficiency comes from $\chi^2 > 5$?")
+    pdf.savefig(fig3)
+    plt.close(fig3)
 
-
+    fig4, ax4 = plt.subplots(figsize=(6,5))
+    norm = mpl.colors.Normalize(vmin=bib_options.min(), vmax=bib_options.max())
+    cmap = plt.cm.viridis 
+    for option in bib_options:
+        print(all_eff_data[option]["eta_err"])
+        color=cmap(norm(option))
+        alpha = 0.9-0.6*norm(option)
+        ax4.errorbar(all_eff_data[option]["eta_centers"], all_eff_data[option]["eta_eff"], yerr=all_eff_data[option]["eta_err"], label=option, color=color, alpha=alpha)
+    ax4.legend()
+    ax4.set_xlabel("Truth $\eta$")
+    ax4.set_ylabel("Reconstruction efficiency (%)")
+    ax4.set_title("$\eta$-dependent tracking efficiency")
+    pdf.savefig(fig4)
+    plt.close(fig4)
+    
 print(f"plot saved to {plot_path}")
