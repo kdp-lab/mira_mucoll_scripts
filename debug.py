4d38c87108ed55f6c583ed0998180a04bf47fd90
Mira Littmann <miralittmann@ap23.uc.osg-htc.org>
2025-11-04 18:44:52 -0600

    removing some older stuff to clean

--- debug.py
+++
@@ -1,6 +0,0 @@
-import json
-
-file_path = "/scratch/miralittmann/analysis/mira_analysis_code/efficiency/bib/loose/3500_10/3500_10_reco0.json"
-with open(file_path) as file:
-    data = json.load(file)
-    print(data.keys())
